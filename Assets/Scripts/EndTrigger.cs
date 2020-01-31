326a4da8ed5c6ed8e90614c010d13812df1b51a1
JokullFreysteinsson <42611644+JokullFreysteinsson@users.noreply.github.com>
2020-01-31 09:33:23 +0000

    Add files via upload

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameMangaer gameMangaer;
+
+    void OnTriggerEnter ()
+    {
+        gameMangaer.CompleteLevel();
+    }
+}
